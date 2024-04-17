cb983123443d9836aad6110befd7900e909d4d10
José Ricardo <josericardosaraiva7@gmail.com>
2024-04-17 09:58:24 -0300

    configurado a fonte

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from "react-native";
+import { StatusBar,ActivityIndicator } from "react-native";
+import { useFonts, Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter"
 import Home from "./src/screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_700Bold,
+	})
+
   return (
-    <>
+    <>{fontsLoaded ? <Home /> : <ActivityIndicator size={50} />}
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true}/>
-      <Home />
     </>
   );
 }
